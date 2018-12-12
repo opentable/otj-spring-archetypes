ad1272bab30152a039c87615ea511f8003ce1da5
Dmitry Kaukov <dkaukov@opentable.com>
2018-12-12 18:54:36 +1100

    otj-server 3.0 changes (mvc service)

---
+++ service-mvc/src/main/resources/archetype-resources/src/main/java/Main.java
@@ -0,0 +1,50 @@
+## Licensed under the Apache License, Version 2.0 (the "License");
+## you may not use this file except in compliance with the License.
+## You may obtain a copy of the License at
+##
+## http://www.apache.org/licenses/LICENSE-2.0
+##
+## Unless required by applicable law or agreed to in writing, software
+## distributed under the License is distributed on an "AS IS" BASIS,
+## WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+## See the License for the specific language governing permissions and
+## limitations under the License.
+package ${package};
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Import;
+import org.springframework.web.client.RestTemplate;
+
+import com.opentable.server.OTApplication;
+import com.opentable.server.mvc.MVCServer;
+import com.opentable.service.ServiceInfo;
+import com.opentable.service.discovery.client.EnableDiscoveryClient;
+
+import com.opentable.resttemplate.RestTemplateConfiguration;
+import com.opentable.resttemplate.RestTemplateFactory;
+
+@Configuration
+@MVCServer
+@EnableDiscoveryClient
+@Import({RestTemplateConfiguration.class, MyController.class})
+public class Main {
+    public static void main(final String[] args) {
+        OTApplication.run(Main.class, args);
+    }
+
+    @Bean
+    public ServiceInfo serviceInfo() {
+        return new ServiceInfo() {
+            @Override
+            public String getName() {
+                return "${artifactId}";
+            }
+        };
+    }
+
+    @Bean
+    public RestTemplate curlClient(RestTemplateFactory restTemplateFactory) {
+        return restTemplateFactory.newTemplate();
+    }
+}
---
+++ service-mvc/src/main/resources/archetype-resources/src/test/java/MainTest.java
@@ -0,0 +1,43 @@
+## Licensed under the Apache License, Version 2.0 (the "License");
+## you may not use this file except in compliance with the License.
+## You may obtain a copy of the License at
+##
+## http://www.apache.org/licenses/LICENSE-2.0
+##
+## Unless required by applicable law or agreed to in writing, software
+## distributed under the License is distributed on an "AS IS" BASIS,
+## WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+## See the License for the specific language governing permissions and
+## limitations under the License.
+package ${package};
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import javax.inject.Inject;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import  ${package}.Main;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(classes=Main.class, webEnvironment=WebEnvironment.RANDOM_PORT)
+public class MainTest {
+
+    @Inject
+    private TestRestTemplate testRestTemplate;
+
+    @Test
+    public void testHelloWorld() {
+        ResponseEntity<String> actualResponse = testRestTemplate.getForEntity("/", String.class);
+        assertThat(actualResponse.getStatusCode()).isEqualTo(HttpStatus.OK);
+        assertThat(actualResponse.getBody()).isEqualTo("Hello, world!");
+    }
+
+}
